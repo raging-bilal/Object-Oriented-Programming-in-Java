2a6505db4a36d4b64dd0b27bbb131778ec7e085b
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-04-20 10:03:22 +0530

    Add files via upload

---
+++ customClass.java
@@ -0,0 +1,25 @@
+class Employee{
+    int id;
+    String name;
+
+}
+
+public class customClass{
+    public static void main(String[] args) {
+
+        System.out.println("This is my custom class!");
+
+        Employee Harry=new Employee();
+
+        Harry.id=5;
+        Harry.name="Code With Harry";
+
+        System.out.println(Harry.id);
+        System.out.println(Harry.name);
+
+
+
+
+        
+    }
+}
